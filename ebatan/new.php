f8d4e5f7184aea92199b48475d8f4a6442c374ed
ebara10 <chihiro.ebara@net-marketing.co.jp>
2015-01-30 17:37:34 +0900

    insert ok

--- ebatan/new.php
+++ ebatan/new.php
@@ -1,34 +1,25 @@
 <?php
-
-//データベースに接続
-$link = mysql_connect('localhost', 'user_b', 'ned5725');
-if (!$link) {
-    die('接続失敗です。'.mysql_error());
-}
-
-//print('<p>接続に成功しました。</p>');
-
-//データベースの選択
-$db_selected = mysql_select_db('bbs', $link);
-if(!$db_selected){
-	die("データベースの選択失敗です".mysql_error());
-}
-//print('<p>データベースの選択に成功しました</p>');
-
-//SQLを格納
-$sql = "INSERT INTO post (
-) VALUES (?, ?, ?, ?)";
-
-//クエリの発行
-$result = mysql_query($sql, $link);
-if(!$result){
-	die('<p>クエリの発行に失敗しました。。</p>');
-}
-
-$close_flag = mysql_close($link);
-
-if ($close_flag){
-	//print('<p>切断に成功しました。</p>');
-}
-
+//デバッグ用
+error_reporting(E_ALL);
+ini_set( 'display_errors', 1 );
 ?>
+
+<html>
+<head>
+<title>新規投稿</title>
+</head>
+<body>
+<form action="new_kakunin.php" method="post">
+<p>
+件名：<input type="text" name="title">
+</p>
+<p>
+本文：<textarea name="body" rows="4" cols="40">ここに本文</textarea>
+</p>
+<p>
+<input type="submit" value="送信">
+</p>
+</form>
+
+</body>
+</html>
